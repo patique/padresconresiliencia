f561b9340860b21ee4dd165f7f3d0cea0e30127e
Pablo Tinoco <pablotinocoquevedo@gmail.com>
2026-01-15 00:37:58 +0100

    Refactor: Move import to top of file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -51,7 +52,7 @@ export const metadata: Metadata = {
   },
 };
 
-import Script from "next/script";
+
 
 export default function RootLayout({
   children,
